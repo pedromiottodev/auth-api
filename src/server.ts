caa29e271ec55d5d18fbceafca5b53a4cd65ff35
Pedro Miotto <pedromiottodev@gmail.com>
2026-01-05 00:12:05 -0300

    config: uso de variáveis de ambiente com dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express"
+import "dotenv/config"
 
 const app = express()
+const port = Number(process.env.PORT) || 3000
 
-app.listen(3000, () => {
+
+app.listen(port, () => {
   console.log("SERVIDOR ONLINE")
 });
